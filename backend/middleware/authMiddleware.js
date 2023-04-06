4509c45f712c9d5f9ba6cdc42f9d953f3f5e68ff
Brandon Bryan <bshwag@gmail.com>
2023-04-06 16:55:40 -0400

    Fixed typo

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,7 +8,7 @@ const authorizeUser = asyncHandler(async (req, res, next) => {
 	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
 		try {
 			// Get token from header
-			token = req.header.authorization.split(' ')[1];
+			token = req.headers.authorization.split(' ')[1];
 
 			// Verify token
 			const decoded = jwt.verify(token, process.env.JWT_SECRET);
