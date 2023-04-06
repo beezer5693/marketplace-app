e7b9be1b43dd3bdba5b3ee2d723469f0c80ec01e
Brandon Bryan <bshwag@gmail.com>
2023-04-06 16:39:13 -0400

    Added comments

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT;
 
+// Connect database
 connectDB();
 
+// Init express
 const app = express();
 
 app.use(express.json());
@@ -12,5 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use('/users', require('./routes/userRoutes'));
+
+// middleware
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
