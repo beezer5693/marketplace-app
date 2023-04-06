4d1ad93219594dfd836edf6042fc80f03ccc7b89
Brandon Bryan <bshwag@gmail.com>
2023-04-06 16:34:26 -0400

    Updated register function to include token and added login function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+const { generateToken } = require('../utils/generateToken');
 
 // @desc    Register a new user
 // @route   /api/users/register
@@ -22,18 +22,44 @@ const registerUser = asyncHandler(async (req, res) => {
 	const salt = await bcrypt.genSalt(10);
 	const hashedPassword = await bcrypt.hash(password, salt);
 
-	const user = {
+	const user = await User.create({
 		name,
 		email,
 		password: hashedPassword
-	};
+	});
 
-	await User.create(user);
-
-	res.status(201).json(user);
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			token: generateToken(user._id)
+		});
+	} else {
+		res.status(400).json({ message: 'Invalid user data' });
+	}
 });
 
-const loginUser = asyncHandler(async (req, res) => {});
+// @desc    Login a user
+// @route   /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email });
+
+	if (user && (await bcrypt.compare(password, user.password))) {
+		res.status(200).json({
+			_id: user.id,
+			name: user.name,
+			email: user.email,
+			token: generateToken(user._id)
+		});
+	} else {
+		res.status(400).json({ message: 'Invalid credentials' });
+	}
+});
+
 const getLoggedInUser = asyncHandler(async (req, res) => {});
 
 module.exports = {
