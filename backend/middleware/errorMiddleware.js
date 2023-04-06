f2e5e847361bd639a9629a43e8b6ba9c69c6834e
Brandon Bryan <bshwag@gmail.com>
2023-04-06 16:37:05 -0400

    Added error handler function to catch errors

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,8 @@
+const errorHandler = (err, res) => {
+	const statusCode = res.statusCode ? res.statusCode : 500;
+	res.status(statusCode).json({
+		message: err.message
+	});
+};
+
+module.exports = { errorHandler };
